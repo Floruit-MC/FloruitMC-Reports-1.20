3150bd088e9f0f1fc61b4a7236e9d20f6d29f73e
Luiz Miguel <134985121+luizfloruit@users.noreply.github.com>
2024-10-16 21:17:10 -0400

    Added

---
+++ src/main/java/com/github/hanielcota/reports/discord/DiscordToken.java
@@ -0,0 +1,10 @@
+package com.github.hanielcota.reports.discord;
+
+import lombok.Getter;
+
+@Getter
+public class DiscordToken {
+
+    private final String tokenDiscord = "MTAxODkzMTM5NzA5Nzk0NzIxNw.GXXNuw.zSoik4-Bxmw4F4CC9GNvgwfbnzEX15wSQiGhFE";
+
+}
